b51449bffc64dca16b8bf821d079ed1b85254988
FukumiKoshikawa <ee37064@meiji.ac.jp>
2017-02-25 04:16:10 +0000

    tr OK

--- style.js
+++ style.js
@@ -37,3 +37,24 @@ $(function () {
        in: { effect: 'flip' ,delay: 70,} ,
    });
 });
+
+//------------------------------------------pagetop-----
+$(function() {
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
